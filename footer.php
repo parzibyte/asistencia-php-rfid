8df4de0c61b85204cde20f326e493e1c37322d8a
Luis Cabrera Benito <parzibyte@gmail.com>
2020-11-30 20:07:37 -0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -28,10 +28,10 @@ También tengo canal de YouTube: https://www.youtube.com/channel/UCroP4BTWjfM0Ck
 */ ?>
 </main>
 <footer class="px-2 py-2 fixed-bottom bg-dark">
-        <span class="text-muted">Attendance system written by
+        <span class="text-muted">Attendance system with RFID tags and PHP. Written by
             <a class="text-white" href="//parzibyte.me/blog">Parzibyte</a>
             &nbsp;|&nbsp;
-            <a target="_blank" class="text-white" href="https://parzibyte.me/blog/2020/11/22/sistema-registro-asistencia-php-mysql/">
+            <a target="_blank" class="text-white" href="https://parzibyte.me/blog/2020/11/28/sistema-registro-asistencia-tarjetas-rfid/">
                 View source
             </a>
         </span>
